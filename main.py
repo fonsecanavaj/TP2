ce82d0cdb03a7134df8c56957ca50e3c918af2a8
fonsecanavaj <143746128+fonsecanavaj@users.noreply.github.com>
2023-09-14 10:19:31 -0400

    Update main.py
    Exercice 1.c

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 # 14/09/23
 # TP2 - exercice 1c
 
-
+# Nombre maximal d'astérix imprimés
 nombre_de_prints = 11
 
-
+# Imprimer une ligne d'astérix
+# À chaque reprise, on diminue le nombre de prints par 1
 while nombre_de_prints > 0:
     print('*' * nombre_de_prints)
     nombre_de_prints -= 1
+    
